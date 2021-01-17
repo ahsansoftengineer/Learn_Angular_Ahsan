cdde5c97454eda3fdfbd1cc8eaa1aebef0af60dc
Muhammad Ahsan <ahsansoftengineer>
2021-01-17 23:07:12 +0500

    Part 27 Services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { BrowserModule} from '@angular/platform-browser'
 import { Component } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module'
+import { AppRoutingModule } from './app-routing.module';
+import { UserService } from './user.service'
+import { isConstructorDeclaration } from 'typescript';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  // Part 24 Routing
-  
+  // Part 27 Services
+  constructor(private user:UserService){
+    console.log(user.getData());
+  }
 }
